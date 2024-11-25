f7a4bb15ffa6159587490ca58def74ff936cf63b
Jan Torres <jan.torres@estudiants.urv.cat>
2024-11-25 17:51:33 +0100

    usuari sense provar

--- src/java/service/UsuariService.java
+++ src/java/service/UsuariService.java
@@ -38,7 +38,7 @@ import model.entities.Usuari;
  * @author USUARIO
  */
 @Path("/customer")
-public class UsuariService {
+public class UsuariService extends AbstractFacade<Usuari>{
     @PersistenceContext(unitName = "Homework1PU")
     private EntityManager em;
     private Map<Integer, Article> articles = new HashMap<>();
@@ -46,7 +46,17 @@ public class UsuariService {
     @Context
     private HttpHeaders headers;
     
+    public UsuariService() {
+        super(Usuari.class);
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
     @GET
+    @Path("/all")
     @Produces({MediaType.APPLICATION_JSON})
     public Collection<Usuari> getAllCustomers(){
         Collection<Usuari> usuaris = new ArrayList<>();
@@ -64,13 +74,11 @@ public class UsuariService {
         return result;
     }
     
-    /*@GET
+    @GET
+    @Path("/{id}")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getCustomerById(@PathParam ("id") int id){
-       String queryText = "SELECT u FROM Usuari u WHERE u.id = :id";
-       TypedQuery<Usuari> query = em.createQuery(queryText, Usuari.class);
-       query.setParameter("id", id);
-       Usuari result = query.getSingleResult();
+       Usuari result = em.find(Usuari.class, id);
        if (result != null) return Response.status(Response.Status.OK).entity(result).build();
         else return Response.status(Response.Status.NOT_FOUND).build();
     }
@@ -79,12 +87,9 @@ public class UsuariService {
     @Secured
     @Consumes({MediaType.APPLICATION_JSON})
     public Response modifyCustomerById(@PathParam ("id") int id, String nom, String dni, int telef, String username){
-       String queryText = "SELECT u FROM Usuari u WHERE u.id = :id";
-       TypedQuery<Usuari> query = em.createQuery(queryText, Usuari.class);
-       query.setParameter("id", id);
-       Usuari u = query.getSingleResult();
+       Usuari u = em.find(Usuari.class, id);
        if (u == null) return Response.status(Response.Status.NOT_FOUND).build();
-       queryText = "UPDATE Usuari SET nom = :nom, dni = :dni, telef = :telef, username = :username WHERE id = :id";
+       String queryText = "UPDATE Usuari SET nom = :nom, dni = :dni, telef = :telef, username = :username WHERE id = :id";
        Query queryMod = em.createQuery(queryText);
        queryMod.setParameter("nom", nom);
        queryMod.setParameter("dni", dni);
@@ -93,6 +98,6 @@ public class UsuariService {
        queryMod.setParameter("id", id);
        if (queryMod.executeUpdate() == 1) return Response.status(Response.Status.OK).build();
        else return Response.status(Response.Status.BAD_REQUEST).build();
-    }*/
+    }
 }
 
